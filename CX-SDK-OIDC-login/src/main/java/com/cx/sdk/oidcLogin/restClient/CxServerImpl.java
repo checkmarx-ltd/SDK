3984ac8ae204b0cbb96149688bf2bd068f52e168
ilandn <ilandayans@gmail.com>
2021-06-04 07:51:47 -0500

    bugid: 2246 - User info request failed due to multiple headers CR_by: N/A

--- CX-SDK-OIDC-login/src/main/java/com/cx/sdk/oidcLogin/restClient/CxServerImpl.java
+++ CX-SDK-OIDC-login/src/main/java/com/cx/sdk/oidcLogin/restClient/CxServerImpl.java
@@ -209,6 +209,8 @@ public class CxServerImpl implements ICxServer {
             disableCertificateValidation(builder);
             client = builder.setDefaultHeaders(headers).build();
             postRequest = RequestBuilder.get()
+                    .addHeader(Consts.AUTHORIZATION_HEADER, Consts.BEARER + accessToken)
+                    .addHeader("Content-Length", "0")
                     .setUri(userInfoURL)
                     .build();
             userInfoResponse = client.execute(postRequest);
