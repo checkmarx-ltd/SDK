73c8ec73972baf71cfffe689cb6d046d54c73632
ilandn <ilandayans@gmail.com>
2021-10-21 06:44:50 -0500

    bugid: 3006 - Fix for TLS support CR_by: N/A

--- CX-SDK-Infrastructure/src/main/java/com/cx/sdk/infrastructure/proxy/ConnectionFactory.java
+++ CX-SDK-Infrastructure/src/main/java/com/cx/sdk/infrastructure/proxy/ConnectionFactory.java
@@ -2,22 +2,23 @@ package com.cx.sdk.infrastructure.proxy;
 
 import com.cx.sdk.application.contracts.providers.SDKConfigurationProvider;
 import com.cx.sdk.domain.entities.ProxyParams;
+import org.apache.http.conn.ssl.TrustAllStrategy;
+import org.apache.http.ssl.SSLContexts;
+import org.apache.http.ssl.TrustStrategy;
 import org.glassfish.jersey.client.HttpUrlConnectorProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import javax.inject.Inject;
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
+import javax.net.ssl.*;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.URL;
 import java.security.KeyManagementException;
+import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
 
 public class ConnectionFactory implements HttpUrlConnectorProvider.ConnectionFactory {
     private final SDKConfigurationProvider sdkConfigurationProvider;
@@ -33,9 +34,10 @@ public class ConnectionFactory implements HttpUrlConnectorProvider.ConnectionFac
         Proxy proxy = null;
         Proxy.Type proxyType = null;
         ProxyParams proxyParams = sdkConfigurationProvider.getProxyParams();
-        if (proxyParams != null && proxyParams.getType() != null){
-            if (proxyParams.getType().equals("HTTPS")){
-                proxyType =Proxy.Type.HTTP;
+        if (proxyParams != null && proxyParams.getType() != null) {
+            if (proxyParams.getType().equalsIgnoreCase("HTTPS")
+                    || proxyParams.getType().equalsIgnoreCase("HTTP")) {
+                proxyType = Proxy.Type.HTTP;
             } else {
                 proxyType = Proxy.Type.valueOf(proxyParams.getType());
             }
@@ -43,34 +45,31 @@ public class ConnectionFactory implements HttpUrlConnectorProvider.ConnectionFac
         } else {
             proxy = Proxy.NO_PROXY;
         }
-        HttpURLConnection con = (HttpURLConnection) url.openConnection(proxy);
-        if (con instanceof HttpsURLConnection) {
+        if (url.getProtocol().equalsIgnoreCase("https")) {
             HttpsURLConnection httpsCon = (HttpsURLConnection) url.openConnection(proxy);
             httpsCon.setHostnameVerifier(getHostnameVerifier());
-            httpsCon.setSSLSocketFactory(getSslContext().getSocketFactory());
+            httpsCon.setSSLSocketFactory(getSSLSocketFactory());
             return httpsCon;
-        } else {
-            return con;
         }
+        HttpURLConnection con = (HttpURLConnection) url.openConnection(proxy);
+        return con;
     }
 
-    public SSLContext getSslContext() {
+    private SSLSocketFactory getSSLSocketFactory() throws SSLException {
+        TrustStrategy acceptingTrustStrategy = new TrustAllStrategy();
+        SSLContext sslContext;
         try {
-            sslContext = SSLContext.getInstance("SSL");
-            sslContext.init(null, new TrustManager[]{new SecureTrustManager()}, new SecureRandom());
-        } catch (KeyManagementException ex) {
-            logger.error(ex.getMessage());
-        } catch (NoSuchAlgorithmException ex) {
-            logger.error(ex.getMessage());
+            sslContext = SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
+        } catch (NoSuchAlgorithmException | KeyStoreException | KeyManagementException e) {
+            throw new SSLException("Fail to set trust all certificate, 'SSLConnectionSocketFactory'", e);
         }
-        return sslContext;
+        return sslContext.getSocketFactory();
     }
 
     private HostnameVerifier getHostnameVerifier() {
         return new HostnameVerifier() {
             @Override
-            public boolean verify(String hostname,
-                                  javax.net.ssl.SSLSession sslSession) {
+            public boolean verify(String hostname, javax.net.ssl.SSLSession sslSession) {
                 return true;
             }
         };
