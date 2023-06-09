1efe2067e5368fd7accde1643d9311f01701b28e
Khant1000 <131154593+Khant1000@users.noreply.github.com>
2023-06-09 11:15:30 +0530

    higher banch

--- CX-SDK-OIDC-login/src/main/java/com/cx/sdk/oidcLogin/restClient/CxServerImpl.java
+++ CX-SDK-OIDC-login/src/main/java/com/cx/sdk/oidcLogin/restClient/CxServerImpl.java
@@ -3,6 +3,13 @@ package com.cx.sdk.oidcLogin.restClient;
 import com.cx.sdk.domain.entities.ProxyParams;
 import com.cx.sdk.oidcLogin.constants.Consts;
 import com.cx.sdk.oidcLogin.dto.AccessTokenDTO;
+import org.apache.http.*;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import org.apache.http.client.methods.*;
+import org.apache.http.entity.StringEntity;
+import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
 import com.cx.sdk.oidcLogin.dto.UserInfoDTO;
 import com.cx.sdk.oidcLogin.exceptions.CxRestClientException;
 import com.cx.sdk.oidcLogin.exceptions.CxRestLoginException;
@@ -64,10 +71,48 @@ public class CxServerImpl implements ICxServer {
     private List<Header> headers = new ArrayList<>();
     private String tokenEndpoint = Consts.SAST_PREFIX + "/identity/connect/token";
     private final String clientName;
+
+    private String restEndpoint =  "/CxRestAPI/";
+
+    private String restUri ;
     private final String userInfoEndpoint = Consts.USER_INFO_ENDPOINT;
     public static final String GET_VERSION_ERROR = "Get Version API not found, server not found or version is older than 9.0";
     private static final String AUTHENTICATION_FAILED = " User authentication failed";
     private static final String INFO_FAILED = "User info failed";
+    private AccessTokenDTO accessTokenDTO;
+    private static final String ORIGIN_HEADER = "cxOrigin";
+    private static final String ORIGIN_URL_HEADER = "cxOriginUrl";
+    private static final String TEAM_PATH = "cxTeamPath";
+
+    public static String getCxOrigin() {
+        return cxOrigin;
+    }
+
+    public static void setCxOrigin(String cxOrigin) {
+        CxServerImpl.cxOrigin = cxOrigin;
+    }
+
+    public static String getCxOriginUrl() {
+        return cxOriginUrl;
+    }
+
+    public static void setCxOriginUrl(String cxOriginUrl) {
+        CxServerImpl.cxOriginUrl = cxOriginUrl;
+    }
+
+    public static String getCxTeam() {
+        return cxTeam;
+    }
+
+    public static void setCxTeam(String cxTeam) {
+        CxServerImpl.cxTeam = cxTeam;
+    }
+
+    private static  String cxOrigin = "cxOrigin";
+    private static  String cxOriginUrl = "cxOriginUrl";
+    private static  String cxTeam = "cxTeamPath";
+    private String rootUri;
+
 
     private static final Logger logger = LoggerFactory.getLogger(CxServerImpl.class);
 
@@ -93,6 +138,7 @@ public class CxServerImpl implements ICxServer {
         this.versionURL = serverURL + VERSION_END_POINT;
         this.clientName = clientName;
         this.proxyParams = proxyParams;
+        this.restUri=serverURL+restEndpoint;
         setClient();
     }
 
@@ -108,6 +154,21 @@ public class CxServerImpl implements ICxServer {
         client = builder.build();
     }
 
+    public AccessTokenDTO getAccessTokenDTO() {
+        return accessTokenDTO;
+    }
+
+    public void setAccessTokenDTO(AccessTokenDTO accessTokenDTO) {
+        this.accessTokenDTO = accessTokenDTO;
+    }
+
+    public String getRootUri() {
+        return rootUri;
+    }
+
+    public void setRootUri(String rootUri) {
+        this.rootUri = rootUri;
+    }
     public String getServerURL() {
         return serverURL;
     }
@@ -244,6 +305,57 @@ public class CxServerImpl implements ICxServer {
         return permissions;
     }
 
+    public <T> T putRequest(String relPath, String contentType, String entity, Class<T> responseType, int expectStatus, String failedMsg, StandardCharsets charsets) throws IOException {
+        HttpPut put = new HttpPut(rootUri + relPath);
+        StringEntity entity1 = new StringEntity(entity, charsets.toString());
+        return request(put, contentType, entity1, responseType, expectStatus, failedMsg, false, true);
+    }
+    private <T> T request(HttpRequestBase httpMethod, String contentType, HttpEntity entity, Class<T> responseType, int expectStatus, String failedMsg, boolean isCollection, boolean retry) throws IOException {
+        if (contentType != null) {
+            httpMethod.addHeader("Content-type", contentType);
+        }
+        if (entity != null && httpMethod instanceof HttpEntityEnclosingRequestBase) { //Entity for Post methods
+            ((HttpEntityEnclosingRequestBase) httpMethod).setEntity(entity);
+        }
+        HttpResponse response = null;
+        int statusCode = 0;
+
+        try {
+            httpMethod.addHeader(ORIGIN_HEADER, getCxOrigin());
+            httpMethod.addHeader(ORIGIN_URL_HEADER, getCxOriginUrl());
+            httpMethod.addHeader(TEAM_PATH, getCxTeam());
+            if (accessTokenDTO.getAccessToken()!= null) {
+                httpMethod.addHeader(HttpHeaders.AUTHORIZATION, accessTokenDTO.getTokenType() + " " + accessTokenDTO.getAccessToken());
+            }
+
+            response = client.execute(httpMethod);
+            statusCode = response.getStatusLine().getStatusCode();
+
+            if (statusCode == HttpStatus.SC_UNAUTHORIZED) { // Token has probably expired
+                throw new CxRestClientException("Failed to Authenticate Token");
+            }
+
+            validateResponse(response, expectStatus, "Failed to " + failedMsg);
+
+            //extract response as object and return the link
+            return convertToObject(response, responseType);
+        } catch (UnknownHostException e) {
+            logger.error(e.getMessage());
+            try {
+                throw new CxRestClientException("Connection to checkMarx server failed "+e.getMessage());
+            } catch (CxRestClientException ex) {
+                throw new RuntimeException(ex);
+            }
+
+        } catch (CxRestClientException e) {
+            throw new RuntimeException(e);
+        } catch (CxValidateResponseException e) {
+            throw new RuntimeException(e);
+        } finally {
+            httpMethod.releaseConnection();
+            HttpClientUtils.closeQuietly(response);
+        }
+    }
     private Permissions getPermissions(UserInfoDTO jsonResponse) {
         ArrayList<String> sastPermissions = jsonResponse.getSastPermissions();
         return new Permissions(sastPermissions.contains(Consts.SAVE_SAST_SCAN), sastPermissions.contains(Consts.MANAGE_RESULTS_COMMENT),
@@ -321,6 +433,40 @@ public class CxServerImpl implements ICxServer {
         cb.setProxyAuthenticationStrategy(new ProxyAuthenticationStrategy());
     }
 
+    private static <T> T convertToObject(HttpResponse response, Class<T> responseType) throws CxRestClientException {
+        ObjectMapper mapper = getObjectMapper();
+        try {
+            if (responseType != null && responseType.isInstance(response)){
+                return (T) response;
+            }
+
+
+            // If the caller is asking for the whole response, return the response (instead of just its entity),
+            // no matter if the entity is empty.
+            if (responseType != null && responseType.isAssignableFrom(response.getClass())) {
+                return (T) response;
+            }
+            if (response.getEntity() == null) {
+                return null;
+            }
+            String json = IOUtils.toString(response.getEntity().getContent(), Charset.defaultCharset());
+            if (responseType.equals(String.class)) {
+                return (T) json;
+            }
+            return mapper.readValue(json,responseType );
+
+        } catch (IOException e) {
+            throw new CxRestClientException("Failed to parse json response: " + e.getMessage());
+        }
+    }
+
+    private static ObjectMapper getObjectMapper() {
+        ObjectMapper result = new ObjectMapper();
+
+        // Prevent UnrecognizedPropertyException if additional fields are added to API responses.
+        result.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return result;
+    }
     private HttpClientBuilder disableCertificateValidation(HttpClientBuilder builder) {
         try {
             SSLContext disabledSSLContext = SSLContexts.custom().loadTrustMaterial((x509Certificates, s) -> true).build();
