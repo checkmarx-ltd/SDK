372c6a8adb0519b304f9dd474e680a56e3448393
Khant1000 <131154593+Khant1000@users.noreply.github.com>
2023-08-08 16:27:58 +0530

    property set

--- CX-SDK-OIDC-login/src/main/java/com/cx/sdk/oidcLogin/webBrowsing/OIDCWebBrowser.java
+++ CX-SDK-OIDC-login/src/main/java/com/cx/sdk/oidcLogin/webBrowsing/OIDCWebBrowser.java
@@ -178,17 +178,19 @@ public class OIDCWebBrowser extends JFrame implements IOIDCWebBrowser {
     }
 
     public static Engine defaultEngine() {
+
         if (ENGINE == null || ENGINE.isClosed()) {
-            if(isMac()){
+            if(System.getProperty("RenderingMode")!=null && !System.getProperty("RenderingMode").isEmpty() && System.getProperty("RenderingMode").equalsIgnoreCase("OFF_SCREEN")) {
+                ENGINE = Engine.newInstance(EngineOptions
+                            .newBuilder(RenderingMode.OFF_SCREEN)
+                            .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36")
+                            .addSwitch("--disable-google-traffic")
+                            .licenseKey(JxBrowserLicense.getLicense())
+                            .build());
+
+            }else {
                 ENGINE = Engine.newInstance(EngineOptions
                         .newBuilder(RenderingMode.OFF_SCREEN)
-                        .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36")
-                        .addSwitch("--disable-google-traffic")
-                        .licenseKey(JxBrowserLicense.getLicense())
-                        .build());
-            }else{
-                ENGINE = Engine.newInstance(EngineOptions
-                        .newBuilder(RenderingMode.HARDWARE_ACCELERATED)
                         .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36")
                         .addSwitch("--disable-google-traffic")
                         .licenseKey(JxBrowserLicense.getLicense())
